80500c8d1b9864c1693dbb90e74169f758567f10
Dragan Nikolic <dragan@fintechdev.net>
2017-03-01 16:51:35 -0800

    implement file size watcher with error

--- 02_event_emitters/01_read_large_file.js
+++ 02_event_emitters/01_read_large_file.js
@@ -1,17 +1,20 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
+var fs = require('fs')
 
-var stream = fs.createReadStream('/home/dragan/Downloads/xampp-linux-x64-5.6.24-1-installer.run');
+//const filename = '/home/dragan/Downloads/xampp-linux-x64-5.6.24-1-installer.run'  // Home Linux
+const filename = '/Users/dnikolic/Downloads/Win8.1_English_x32.iso'  // Mac
 
-var chunk_counter = 1;
+var stream = fs.createReadStream(filename)
+
+var chunk_counter = 1
 
 stream.on('data', function(data) {
     //console.log('Received data: ' + data);
-    console.log('Received data: ' + chunk_counter);
-    chunk_counter++;
-});
+    console.log('Received data: ' + chunk_counter + " len:" + data.length)
+    chunk_counter++
+})
 
 stream.on('end', function() {
-    console.log('End of file has been reached');
-});
+    console.log('End of file has been reached')
+})
---
+++ 02_event_emitters/02_file_size_watcher/file_size_watcher.spec.js
@@ -0,0 +1,54 @@
+// to run: ./node_modules/jasmine-node/bin/jasmine-node ./file_size_watcher.spec.js
+'use strict'
+
+var FilesizeWatcher = require('./file_size_watcher')
+var exec = require('child_process').exec
+
+describe('FilesizeWatcher', function() {
+    var watcher
+    afterEach(function() { 
+        watcher.stop()
+    })
+
+    it('should fire a "grew" event when the file grew in size', function(done) {
+
+        var path = '/var/tmp/filesizewatcher.test'
+        exec('rm -f ' + path + ' ; touch ' + path, function() {
+            watcher = new FilesizeWatcher(path)
+            
+            watcher.on('grew', function(gain) { 
+                expect(gain).toBe(5)
+                done()
+            })
+
+            exec('echo "test" > ' + path, function() {})
+        })
+    })
+
+    it('should fire a "shrank" event when the file grew in size', function(done) {
+
+        var path = '/var/tmp/filesizewatcher.test'
+        exec('rm -f ' + path + ' ; echo "test" > ' + path, function() {
+            watcher = new FilesizeWatcher(path)
+            
+            watcher.on('shrank', function(loss) { 
+                expect(loss).toBe(3)
+                done()
+            })
+
+            exec('echo "a" > ' + path, function() {})
+
+        }) 
+    })
+
+    it('should fire "error" if path does not start with a slash', function(done) { 
+        
+        var path = 'var/tmp/filesizewatcher.test'
+        watcher = new FilesizeWatcher(path)
+
+        watcher.on('error', function(err) { 
+            expect(err).toBe('Path does not start with a slash')
+            done()
+        })
+    })
+})
